e36f7eff1b262a655e436d0b65dd999cdd9dec0c
7usseinel8areb <halghryb287@gmail.com>
2025-11-13 00:49:53 +0200

    Add DTOs

---
+++ DemoECommerce.AuthenticationApiSolution/AuthenticationApi.Application/DTOs/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationApi.Application.DTOs;
+
+public record LoginDTO(
+    [Required,EmailAddress]string Email,
+    [Required]string Password
+);
