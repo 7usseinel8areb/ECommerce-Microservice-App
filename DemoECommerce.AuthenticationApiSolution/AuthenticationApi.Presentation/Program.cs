3cb5d1eb72771306c511f0a03b4794c8d225d71e
7usseinel8areb <halghryb287@gmail.com>
2025-11-13 00:52:39 +0200

    Edit configurations

---
+++ DemoECommerce.AuthenticationApiSolution/AuthenticationApi.Presentation/Program.cs
@@ -0,0 +1,31 @@
+using AuthenticationApi.Application.DependencyInjection;
+using AuthenticationApi.Infrastructure.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseInfrastructurePolicy();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
