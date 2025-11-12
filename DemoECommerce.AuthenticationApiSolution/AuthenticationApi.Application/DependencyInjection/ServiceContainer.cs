74048af751b3493852ef7e1adf3d533804299570
7usseinel8areb <halghryb287@gmail.com>
2025-11-13 00:49:43 +0200

    Add DI of Application Layer

---
+++ DemoECommerce.AuthenticationApiSolution/AuthenticationApi.Application/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,23 @@
+﻿using AuthenticationApi.Application.Services.Implementation;
+using AuthenticationApi.Application.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthenticationApi.Application.DependencyInjection;
+
+public static class ServiceContainer
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // Register all services in Application layer
+        //services.Scan(scan => scan
+        //    .FromAssemblyOf<ApplicationServiceContainer>()
+        //    .AddClasses(classes => classes.InNamespaces("AuthenticationApi.Application.Services"))
+        //    .AsImplementedInterfaces()
+        //    .WithScopedLifetime());
+
+        services.AddScoped<IUserService, UserService>();
+
+        return services;
+    }
+
+}
