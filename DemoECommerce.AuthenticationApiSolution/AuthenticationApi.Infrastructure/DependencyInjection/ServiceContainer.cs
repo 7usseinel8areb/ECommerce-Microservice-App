0ab4cf2fda802de1ae99eaba26e6c84e47d3b914
7usseinel8areb <halghryb287@gmail.com>
2025-11-13 00:53:11 +0200

    Infrastructure DI

---
+++ DemoECommerce.AuthenticationApiSolution/AuthenticationApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,41 @@
+﻿using AuthenticationApi.Application.Interfaces;
+using AuthenticationApi.Application.Services.Interfaces;
+using AuthenticationApi.Infrastructure.Data;
+using AuthenticationApi.Infrastructure.Repositories;
+using AuthenticationApi.Infrastructure.Services;
+using eCommerce.SharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthenticationApi.Infrastructure.DependencyInjection;
+
+public static class ServiceContainer
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+    {
+
+        // Add db connectivity
+        // Jwt Add auth scheme
+        SharedService.AddSharedServices<AuthenticationDbContext>(services, configuration, configuration["MySerilog:FileName"]!);
+
+        // Create Depedency injection for Repositories
+        services.AddScoped<IUser, UserRepository>();
+
+        // Create Depedency injection for Services
+        services.AddScoped<IPasswordService, PasswordService>();
+        services.AddScoped<ITokenService, TokenService>();
+
+        return services;
+    }
+
+
+    public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+    {
+        // Register middlewares such as:
+        // Global exception
+        // Listen to only api gateway calls
+        SharedService.UseSharedPolicies(app);
+        return app;
+    }
+}
